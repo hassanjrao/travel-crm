026d75332ebf5730cd6b8f95a54a65c30a0004f3
Hassan <hassanjrao@gmail.com>
2026-06-03 16:11:01 +0500

    init commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,5 @@
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaClient } = require("@prisma/client");
 
 function createPrismaClient() {
   const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
@@ -10,11 +9,10 @@ function createPrismaClient() {
   });
 }
 
-type PrismaClientInstance = ReturnType<typeof createPrismaClient>;
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient>;
+};
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClientInstance };
-
-export const prisma: PrismaClientInstance =
-  globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
